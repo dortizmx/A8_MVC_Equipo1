707a1094253f6be8f4f93e1b9e485a5d0b75f7c2
David ORTIZ <david.ortiz@geodis.com>
2021-06-21 11:56:34 -0500

    Agregar archivos de proyecto.

---
+++ A8_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace A8_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
