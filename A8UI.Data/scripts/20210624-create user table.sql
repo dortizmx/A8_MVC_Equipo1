7d7d23b30f8f00101d13233baa5dbd43460820bc
David ORTIZ <david.ortiz@geodis.com>
2021-06-24 23:16:07 -0500

    add script

---
+++ A8UI.Data/scripts/20210624-create user table.sql
@@ -0,0 +1,13 @@
+﻿create table users (
+id int primary key identity not null,
+email varchar(255) not null unique,
+nombre varchar(255) not null,
+apellido_paterno varchar(255) not null,
+apellido_materno varchar(255) not null,
+contrasena varchar(100) not null,
+status varchar(2) not null default 'AC'
+)
+go
+
+select *
+  from users;
