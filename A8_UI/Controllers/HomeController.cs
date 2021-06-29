a910d62444c3a8e92a2a8b88a674d13f2c301ca5
David ORTIZ <david.ortiz@geodis.com>
2021-06-29 16:14:35 -0500

    add screens

--- A8_UI/Controllers/HomeController.cs
+++ A8_UI/Controllers/HomeController.cs
@@ -33,5 +33,14 @@ namespace A8_UI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AgregaPaciente()
+        {
+            return View();
+        }
+        public IActionResult AgregarCita()
+        {
+            return View();
+        }
     }
 }
